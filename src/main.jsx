10586c36b169f4646f2224e2429fd9bd41c6ce6d
VasquezMichael <vasquez.michael.steven@gmail.com>
2023-08-03 21:42:33 -0300

    Probando sincronizacion con el host

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <>
       <App />
+      <p>Testeando sincronizacion con el host</p>
     </>
   </React.StrictMode>
 );
